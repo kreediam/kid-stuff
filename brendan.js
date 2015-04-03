4bf08e20ed835abe924b12d2296df61f8c36bf38
Kevin Reed <kntreed@gmail.com>
2015-04-02 20:34:52 -0400

    hi there im brendan

---
+++ brendan.js
@@ -0,0 +1,54 @@
+var readline = require('readline');
+var number1, number2;
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Hello, what is your last name? ', getFirst);
+
+function getFirst(name){
+  console.log("Nice to know your last name?");
+	rl.question('what is your first name ', ffff);
+}
+
+function ffff(first){
+	console.log("wow you don't have to yell. so your first name is " + first + " then. OK.");
+	rl.question('Please enter a number: ', getSecond);
+}
+
+function getSecond(number){
+  number1 = number;
+
+  rl.question('Thanks. Now enter another number: ', divide);
+}
+
+function divide(number){
+  number2 = number;
+
+  console.log(number1, "/", number2, "=", number1 / number2);
+  console.log("Have a nice day!");
+  console.log(" I for got to ask you something");
+  rl.question('what is your favorite video game?',thing);
+  
+  
+  
+  
+  
+  
+
+
+}
+function thing(game){ 
+	console.log("cool video game!");
+
+	
+	rl.close();	
+}
+	
+	
+	
+	
+	
+	
